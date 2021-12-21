10cbfdf5711b25753d274a3e30611140cc843d96
mcansener <mcansener@hotmail.com>
2021-12-21 23:40:18 +0300

    commit fix.

---
+++ MarsRover/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    internal class Program
+    {
+        static List<Rover> rovers = new List<Rover>();
+        static void Main(string[] args)
+        {
+            string input = @"5 5
+                             1 2 N
+                             LMLMLMLMM
+                             3 3 E
+                             MMRMMRMRRM";
+
+            InitializeRovers(input);
+            rovers[0].Move();
+            rovers[1].Move();
+        }
+
+        /// <summary>
+        /// Initialize the rovers.
+        /// </summary>
+        /// <param name="input"></param>
+        private static void InitializeRovers(string input)
+        {
+            string[] strArray = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            if (strArray.Length == 5)
+            {
+                GenerateRoverInfo(strArray[0].Trim(), strArray[1].Trim(), strArray[2].Trim());
+                GenerateRoverInfo(strArray[0].Trim(), strArray[3].Trim(), strArray[4].Trim());
+            }
+            else
+            {
+                throw new ArgumentException("Invalid input");
+            }
+        }
+
+        /// <summary>
+        /// Generate Rover regarding to the given info,order inputs
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="moveOrder"></param>
+        /// <exception cref="ArgumentException"></exception>
+        private static void GenerateRoverInfo(string planeInfo, string info, string moveOrder)
+        {
+            string[] roverInfo = info.Split(' ');
+            if (roverInfo.Length == 3)
+            {
+                Rover rover = new Rover();
+                rover.XLocation = Convert.ToInt32(roverInfo[0]);
+                rover.YLocation = Convert.ToInt32(roverInfo[1]);
+                rover.CurrentDirection = DirectionEnumHelper.GetDirectionEnumReference(Convert.ToChar(roverInfo[2]));
+                rover.MoveOrder = moveOrder;
+
+                var planeArray = planeInfo.Split(' ');
+                if (planeArray.Length == 2)
+                {
+                    rover.XMax = Convert.ToInt32(planeArray[0]);
+                    rover.YMax = Convert.ToInt32(planeArray[1]);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid plane input");
+                }
+                rovers.Add(rover);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid rover input");
+            }
+        }
+    }
+}
---
+++ MarsRover/Rover.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    /// <summary>
+    /// Mars Rover class
+    /// </summary>
+    internal class Rover
+    {
+        internal int XLocation { get; set; }
+        internal int YLocation { get; set; }
+        internal DirectionEnum CurrentDirection { get; set; }
+        internal string MoveOrder { get; set; }
+        internal int XMax { get; set; }
+        internal int YMax { get; set; }
+
+        /// <summary>
+        /// Executes the move order of the rover
+        /// </summary>
+        internal void Move()
+        {
+            var ordersArray = MoveOrder.ToCharArray(); 
+            foreach (var order in ordersArray)
+            {
+                switch (order)
+                {
+                    //L is to move 90 degrees to left from rovers current direction.
+                    case 'L':
+                        //Check if direction value smaller than zero, if it is then change it to proper direction value.
+                        CurrentDirection = (DirectionEnum)(Convert.ToInt32(CurrentDirection) - 1 < 0 ? 4 + (Convert.ToInt32(CurrentDirection) - 1) : Convert.ToInt32(CurrentDirection) - 1);
+                        break;
+                    //R is to move 90 degrees to rigth from rovers current direction.
+                    case 'R':
+                        //Check if direction value bigger than four, if it is then change it to proper direction value.
+                        CurrentDirection = (DirectionEnum)(Convert.ToInt32(CurrentDirection) + 1 >= 4 ? (Convert.ToInt32(CurrentDirection) + 1) % 4 : Convert.ToInt32(CurrentDirection) + 1);
+                        break;
+                    //Move rover according to direction
+                    case 'M':
+                        switch (CurrentDirection)
+                        {
+                            case DirectionEnum.North:
+                                YLocation++;
+                                break;
+                            case DirectionEnum.East:
+                                XLocation++;
+                                break;
+                            case DirectionEnum.South:
+                                YLocation--;
+                                break;
+                            case DirectionEnum.West:
+                                XLocation--;
+                                break;
+                            default:
+                                break;
+                        }
+                        if (YLocation > YMax || XLocation > XMax || YLocation < 0 || XLocation < 0)
+                        {
+                            throw new ArgumentException("Exceeded plane limits");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
