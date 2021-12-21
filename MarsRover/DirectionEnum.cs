6b5f6d5fb353218ed5118f5f575778569b13b5a4
mcansener <mcansener@hotmail.com>
2021-12-21 23:37:57 +0300

    dev

---
+++ MarsRover/DirectionEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    /// <summary>
+    /// Represents the direction of the rover
+    /// </summary>
+    internal enum DirectionEnum
+    {
+        North = 0, 
+        East = 1,
+        South = 2,
+        West = 3
+    }
+}
---
+++ MarsRover/DirectionEnumHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    internal static class DirectionEnumHelper
+    {
+        internal static DirectionEnum GetDirectionEnumReference(char directionCharacter)
+        {
+            switch (directionCharacter)
+            {
+                case 'N': 
+                    return DirectionEnum.North;
+                case 'S':
+                    return DirectionEnum.South;
+                case 'E':
+                    return DirectionEnum.East;
+                case 'W':
+                    return DirectionEnum.West;
+                default:
+                    throw new ArgumentException("Invalid direction");
+            }
+        }
+    }
+}
